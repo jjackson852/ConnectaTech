adab5406ec91783474563d6e6d89261df140b6fd
jordanadamc@gmail.com <trusnto.1>
2015-01-24 18:02:51 -0600

    2nd Compare

--- app/src/main/java/com/notify/app/mobile/ui/MainActivity.java
+++ app/src/main/java/com/notify/app/mobile/ui/MainActivity.java
@@ -36,6 +36,7 @@ import butterknife.Views;
 public class MainActivity extends BootstrapFragmentActivity {
 
     //Testing123
+    //test
 
     @Inject protected BootstrapServiceProvider serviceProvider;
 
