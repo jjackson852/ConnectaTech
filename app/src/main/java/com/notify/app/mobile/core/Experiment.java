ba1ef02bef651dc1958d155d1725f6845aa62846
jrm102 <jrm_102@hotmail.com>
2015-03-14 14:39:43 -0500

    Merge branches 'Development' and 'Jaime' of https://github.com/jjackson852/ConnectaTech into Jaime
    Conflicts:
    	app/src/main/AndroidManifest.xml
    	app/src/main/java/com/notify/app/mobile/ui/AnywallActivity.java
    	app/src/main/res/layout/experiment_items.xml
    	app/src/main/res/values/strings.xml

--- app/src/main/java/com/notify/app/mobile/core/Experiment.java
+++
@@ -1,7 +0,0 @@
-package com.notify.app.mobile.core;
-
-/**
- * Created by Martinez on 2/28/2015.
- */
-public class Experiment {
-}
--- app/src/main/java/com/notify/app/mobile/core/ExperimentService.java
+++
@@ -1,9 +0,0 @@
-package com.notify.app.mobile.core;
-
-/**
- * Created by Martinez on 2/28/2015.
- */
-public interface ExperimentService {
-
-    ExperimentWrapper getExperimentService();
-}
