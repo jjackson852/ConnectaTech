4a217b360c810291e5564965db6623be72100a8c
Adam Jordan <jordanadamc@gmail.com>
2015-04-13 23:19:21 -0500

    Created Profile Image Gallery Activity

--- app/src/main/java/com/notify/app/mobile/ui/EditImageActivity.java
+++ app/src/main/java/com/notify/app/mobile/ui/EditImageActivity.java
@@ -3,18 +3,28 @@ package com.notify.app.mobile.ui;
 import android.annotation.TargetApi;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Toast;
 
 import com.notify.app.mobile.Injector;
 import com.notify.app.mobile.R;
 import com.notify.app.mobile.bootstrapOrigin.ui.BootstrapActivity;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 /**
  * Created by theblackfu on 3/1/2015.
  */
 public class EditImageActivity extends BootstrapActivity {
 
+    private static final int RESULT_LOAD_IMAGE = 1;
     private Photo photo;
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
@@ -22,7 +32,7 @@ public class EditImageActivity extends BootstrapActivity {
     public void onCreate(Bundle savedInstanceState) {
 
 
-        photo = new Photo();
+       // photo = new Photo();
         // requestWindowFeature(Window.FEATURE_NO_TITLE);
         // getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
@@ -33,6 +43,11 @@ public class EditImageActivity extends BootstrapActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        Intent i = new Intent(
+                Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        startActivityForResult(i, RESULT_LOAD_IMAGE);
 
 //        FragmentManager manager = getFragmentManager();
 //        Fragment fragment = manager.findFragmentById(R.id.fragmentContainer);
@@ -41,11 +56,40 @@ public class EditImageActivity extends BootstrapActivity {
 //            fragment = new EditImageActivity();
 //            manager.beginTransaction().add(R.id.fragmentContainer, fragment)
 //                    .commit();
-        }
+        }    @Override
+             protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+
+            // String picturePath contains the path of selected Image
+
+
+        }
+    }
 
     public Photo getCurrentPhoto() {
         return photo;
     }
 
+
+//    private void addPhotoAndReturn(ParseFile photoFile) {
+////        ((EditPhotoActivity) getActivity()).getCurrentPhoto().setPhotoFile(
+////                photoFile);
+//        ParseUser cameraCurrentUser = ParseUser.getCurrentUser();
+//        cameraCurrentUser.put("picturePath", photoFile);
+//        cameraCurrentUser.saveInBackground();
+//
+//    }
 }
--- app/src/main/java/com/notify/app/mobile/ui/ProviderProfileFragment.java
+++ app/src/main/java/com/notify/app/mobile/ui/ProviderProfileFragment.java
@@ -4,9 +4,12 @@ import android.accounts.OperationCanceledException;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.content.Loader;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -41,6 +44,8 @@ import butterknife.InjectView;
 
 public class ProviderProfileFragment extends ItemListFragment2 {
 
+
+    private int RESULT_LOAD_IMAGE  ;
     @Inject
     protected LogoutService logoutService;
     @Inject
@@ -184,9 +189,16 @@ public class ProviderProfileFragment extends ItemListFragment2 {
         selectImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getActivity(), "Add A Photo", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(getActivity(), EditImageActivity.class);
-                startActivity(intent);
+
+                Intent i = new Intent(
+                        Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+
+                //Toast.makeText(getActivity(), "Add A Photo", Toast.LENGTH_SHORT).show();
+//                Intent intent = new Intent(getActivity(), EditImageActivity.class);
+//                intent.putExtra("providerPic", (android.os.Parcelable) providerPhoto);
+//                startActivity(intent);
             }
         });
 
